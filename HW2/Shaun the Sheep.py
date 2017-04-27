471d98ce9c0bb2bf8f2b1e491354885c95a3c010
梨凯英 <LeeKaiYing@users.noreply.github.com>
2017-04-28 00:06:08 +0700

    Delete Shaun the Sheep.py

--- HW2/Shaun the Sheep.py
+++
@@ -1,8 +0,0 @@
-
-sheep_sizes = [5, 7, 300, 90, 24, 50, 75]
-print ("Hello my name is Le and these is my sheep sizes")
-print (sheep_sizes)
-
-sheep_sizes = [5, 7, 300, 90, 24, 50, 75]
-a = max(sheep_sizes)
-print('Now my biggest sheep has size' ,a, 'lets sheer it!')
